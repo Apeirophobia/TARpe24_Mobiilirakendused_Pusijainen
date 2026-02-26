a9a222f0e8a684260e88c6427d9a273a9e2b617b
Apeirophobia <95441228+Apeirophobia@users.noreply.github.com>
2026-02-26 09:58:48 +0200

    RGB Page works

--- TARpe24_Mobiilirakendused_Pusijainen/RGBPage.xaml.cs
+++ TARpe24_Mobiilirakendused_Pusijainen/RGBPage.xaml.cs
@@ -5,9 +5,22 @@ namespace TARpe24_Mobiilirakendused_Pusijainen;
 public partial class RGBPage : ContentPage
 {
 	Frame colorFrame;
+
+	Frame rFrame;
 	Slider rSlider;
+	Label rLabel;
+
+	Frame gFrame;
 	Slider gSlider;
+	Label gLabel;
+
+	Frame bFrame;
 	Slider bSlider;
+	Label bLabel;
+
+	Stepper alphaStepper;
+	Label aLabel;
+
 	AbsoluteLayout al;
 	public RGBPage()
 	{
@@ -17,36 +30,146 @@ public partial class RGBPage : ContentPage
 		{
 			BackgroundColor = Colors.LightGray
 		};
+
+		rFrame = new Frame
+		{
+			WidthRequest = 20,
+			HeightRequest = 20,
+			BackgroundColor = Colors.LightGray
+		};
+
 		rSlider = new Slider
 		{
 			Minimum = 0,
 			Maximum = 255,
-			HorizontalOptions = LayoutOptions.Center
+			HorizontalOptions = LayoutOptions.Center,
+			WidthRequest = 200
 		};
 
-		gSlider = new Slider
+		rLabel = new Label
+		{
+			FontSize = 12,
+			WidthRequest = 50,
+            Text = $"Red: {rSlider.Value}"
+        };
+
+        gFrame = new Frame
+        {
+            WidthRequest = 20,
+            HeightRequest = 20,
+            BackgroundColor = Colors.LightGray
+        };
+
+        gSlider = new Slider
 		{
 			Minimum = 0,
 			Maximum = 255,
-			HorizontalOptions = LayoutOptions.Center
+			HorizontalOptions = LayoutOptions.Center,
+			WidthRequest = 200
 		};
 
-		bSlider = new Slider
+        gLabel = new Label
+        {
+            FontSize = 12,
+            WidthRequest = 50,
+            Text = $"Green: {gSlider.Value}"
+        };
+
+
+        bFrame = new Frame
+        {
+            WidthRequest = 20,
+            HeightRequest = 20,
+            BackgroundColor = Colors.LightGray
+        };
+
+        bSlider = new Slider
 		{
 			Minimum = 0,
 			Maximum = 255,
-			HorizontalOptions = LayoutOptions.Center
+			HorizontalOptions = LayoutOptions.Center,
+			WidthRequest = 200
 		};
 
-		al = new AbsoluteLayout { Children = { colorFrame, rSlider, gSlider, bSlider } };
-		List<View> controls = new List<View> { colorFrame, rSlider, gSlider, bSlider };
+        bLabel = new Label
+        {
+            FontSize = 12,
+            WidthRequest = 50,
+            Text = $"Blue: {bSlider.Value}"
+        };
+
+		alphaStepper = new Stepper
+		{
+			Minimum = 0,
+			Maximum = 255,
+			Increment = 5,
+			Value = 255,
+		};
+
+		aLabel = new Label
+        {
+            FontSize = 12,
+            WidthRequest = 50,
+            Text = $"Alpha: {alphaStepper.Value}"
+        };
+
+        rSlider.ValueChanged += SliderValueChanged;
+		gSlider.ValueChanged += SliderValueChanged;
+		bSlider.ValueChanged += SliderValueChanged;
+		alphaStepper.ValueChanged += SliderValueChanged;
+		
+
+		al = new AbsoluteLayout { Children = { colorFrame, alphaStepper, aLabel, rFrame, rSlider, rLabel, gFrame, gSlider, gLabel, bFrame, bSlider, bLabel,  } };
+		List<View> controls = new List<View> { colorFrame, alphaStepper, rSlider, gSlider, bSlider,  };
+		List<View> frames = new List<View> { colorFrame, aLabel, rFrame, gFrame, bFrame };
+		List<View> outputLabels = new List<View> { colorFrame, aLabel, rLabel, gLabel, bLabel};
 		for (int i = 0; i < controls.Count; i++)
 		{
-			double yKoht = 0.2 + (double)i * 0.2;
+			double yKoht = 0.15 + (double)i * 0.15;
 			AbsoluteLayout.SetLayoutBounds(controls[i], new Rect(0.5, yKoht, 300, 60));
 			AbsoluteLayout.SetLayoutFlags(controls[i], AbsoluteLayoutFlags.PositionProportional);
-		}
+
+			if (i > 0)
+			{
+				if (i > 1)
+				{
+                    AbsoluteLayout.SetLayoutBounds(frames[i], new Rect(0.4, yKoht - 0.02, 50, 60));
+                    AbsoluteLayout.SetLayoutFlags(frames[i], AbsoluteLayoutFlags.PositionProportional);
+                }
+				
+
+				AbsoluteLayout.SetLayoutBounds(outputLabels[i], new Rect(0.5, yKoht + 0.05, 100, 60));
+                AbsoluteLayout.SetLayoutFlags(outputLabels[i], AbsoluteLayoutFlags.PositionProportional);
+
+            }
+        }
 		Content = al;
 
 	}
+
+	public void SliderValueChanged(object? sender, ValueChangedEventArgs e)
+	{
+		colorFrame.BackgroundColor = Color.FromRgba((int)rSlider.Value, (int)gSlider.Value, (int)bSlider.Value, (int)alphaStepper.Value);
+		if (sender == rSlider)
+		{
+            rLabel.Text = String.Format("Red: {0}", (int)e.NewValue);
+			rFrame.BackgroundColor = Color.FromRgb((int)e.NewValue, 0, 0);
+        }
+		if (sender == gSlider)
+		{
+            gLabel.Text = String.Format("Green: {0}", (int)e.NewValue);
+			gFrame.BackgroundColor = Color.FromRgb(0, (int)e.NewValue, 0);
+        }
+		if (sender == bSlider)
+		{
+            bLabel.Text = String.Format("Blue: {0}", (int)e.NewValue);
+			bFrame.BackgroundColor = Color.FromRgb(0, 0, (int)e.NewValue);
+        }
+
+		if (sender == alphaStepper)
+		{
+			aLabel.Text = String.Format("Alpha: {0}", (int)e.NewValue);
+		}
+
+    }
 }
